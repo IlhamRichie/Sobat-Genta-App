df046fd595dd14318abe9069953bb577f7cadace
IlhmRgn <117716197+IlhamRichie@users.noreply.github.com>
2025-09-15 16:39:48 +0700

    Refactor code structure for improved readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init(); 
   runApp(
     GetMaterialApp(
       title: "Application",
